b111bc51a901399c6f2b708488dfcea7322867fc
Gnlow <entrydark@gmail.com>
2020-02-04 20:57:39 +0900

    v0.1.0: init

--- index.ts
+++ index.ts
@@ -2,17 +2,22 @@ import {
     Pack, Block, Literal, Type, Dict
 } from "dalkak";
 
+declare var TextEncoder: any;
+declare var crypto: any;
+
 export default new Pack({
-    name: "Sample",
+    name: "hash",
     blocks: {
-        log: new Block({
-            name: "log",
-            template: "(text) 찍기",
-            func: (param, project, platform) => {
-                console.log(param.text);
-            },
-            params: {
-                text: Literal.from("안녕")
+        generateHash: new Block({
+            name: "generateHash",
+            template: "((data)로 Hash 생성하기)",
+            func: async (param, project, platform) => {
+                const message = param.data;
+                const msgUint8 = new TextEncoder().encode(message);                           // encode as (utf-8) Uint8Array
+                const hashBuffer = await crypto.subtle.digest('SHA-256', msgUint8);           // hash the message
+                const hashArray = Array.from(new Uint8Array(hashBuffer));                     // convert buffer to byte array
+                const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join(''); // convert bytes to hex string
+                return hashHex;
             }
         })
     }
